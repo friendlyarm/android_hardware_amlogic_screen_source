fa98392c692369a935fce4bb68f07f369d186df0
hmz007 <hmz007@gmail.com>
2016-12-01 13:53:51 +0800

    LOCAL_MODULE: `amlogic' ==> $(TARGET_DEVICE)
    Signed-off-by: hmz007 <hmz007@gmail.com>

--- Android.mk
+++ Android.mk
@@ -5,23 +5,28 @@
 LOCAL_PATH := $(call my-dir)
 
 # HAL module implemenation, not prelinked and stored in
-# /system/lib/hw/screen_source.amlogic.so
+# /system/lib/hw/screen_source.$(TARGET_DEVICE).so
 include $(CLEAR_VARS)
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := aml_screen.cpp v4l2_vdin.cpp
 
 MESON_GRALLOC_DIR ?= hardware/amlogic/gralloc
 
-LOCAL_C_INCLUDES += frameworks/native/include/utils \
-					frameworks/native/include/android \
-					system/core/include/utils \
-					system/core/libion/include \
-					system/core/libion/kernel-headers \
-					$(MESON_GRALLOC_DIR)
+LOCAL_SRC_FILES := \
+    aml_screen.cpp v4l2_vdin.cpp
 
-LOCAL_SHARED_LIBRARIES:= libutils liblog libui
+LOCAL_C_INCLUDES += \
+    frameworks/native/include/utils \
+    frameworks/native/include/android \
+    system/core/include/utils \
+    system/core/libion/include \
+    system/core/libion/kernel-headers \
+    $(MESON_GRALLOC_DIR)
 
-LOCAL_MODULE := screen_source.amlogic
-LOCAL_CFLAGS:= -DLOG_TAG=\"screen_source\"
+LOCAL_CFLAGS := -DLOG_TAG=\"screen_source\"
+
+LOCAL_SHARED_LIBRARIES := libutils liblog libui
+
+LOCAL_MODULE := screen_source.$(TARGET_DEVICE)
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
+
 include $(BUILD_SHARED_LIBRARY)
